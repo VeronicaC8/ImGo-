a873feefe3d4da1de9624e834c8ff3b165caa1d2
Veronica C <svcl_svcl@hotmail.com>
2018-06-27 13:55:03 -0600

    Imagen galeria y base Sitio nuevo campo imagen

--- app/src/main/java/com/example/veronica/imgo/sitioInsertActivity.java
+++ app/src/main/java/com/example/veronica/imgo/sitioInsertActivity.java
@@ -1,13 +1,27 @@
 package com.example.veronica.imgo;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class sitioInsertActivity extends AppCompatActivity {
@@ -24,6 +38,9 @@ public class sitioInsertActivity extends AppCompatActivity {
     EditText editLatitud;
     EditText editLongitud;
 
+    Button btnFoto;
+    ImageView imgFoto;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitio_insert);
@@ -37,7 +54,18 @@ public class sitioInsertActivity extends AppCompatActivity {
         editDireccion = (EditText) findViewById(R.id.editDireccion);
         editLatitud =(EditText) findViewById(R.id.editLatitud);
         editLongitud = (EditText) findViewById(R.id.editLongitud);
+
+        imgFoto=(ImageView) findViewById(R.id.imgFoto);
+        btnFoto=(Button)findViewById(R.id.btnFoto);
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoOpciones();
+            }
+        });
     }
+
+
     public void insertarSitio(View v) {
 
             Integer idSitio = Integer.valueOf(editIdSitio.getText().toString());
@@ -84,8 +112,50 @@ public class sitioInsertActivity extends AppCompatActivity {
      //       editPrecioMax.setText("");
      //       editPrecioMin.setText("");
      //       editDireccion.setText("");
-
+     //       editLatitud.setText("");
+     //       editLongitud.setText("");
 
     }
+
+    private void cargarImagen(){
+        Intent intent =new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione una aplicacion"),10);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode==RESULT_OK){
+            Uri path=data.getData();
+            imgFoto.setImageURI(path);
+        }
+    }
+
+    private void mostrarDialogoOpciones(){
+        final CharSequence[] opciones={"Tomar Foto", "Elegir de Galeria","Cancelar"};
+        final AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Elegir una Opcion: ");
+
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")){
+                    //   abrirCamara();
+                    Intent camaraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                }else {
+                    if (opciones[i].equals("Elegir de Galeria")){
+                        cargarImagen();
+                    }else {
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+    }
+
+  //  file.mkdirs();
+
 
 }
