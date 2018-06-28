fa70acb07b4e09627e48986d562e2ece32202845
Veronica C <svcl_svcl@hotmail.com>
2018-06-28 07:10:53 -0600

    Publicar en Facebook
    Modificacion de Fragment Categoria

--- app/src/main/java/com/example/veronica/imgo/CategoriaFragment.java
+++ app/src/main/java/com/example/veronica/imgo/CategoriaFragment.java
@@ -3,6 +3,7 @@ package com.example.veronica.imgo;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,6 +36,7 @@ public class CategoriaFragment extends Fragment {
         Button btn_bar = (Button) view.findViewById(R.id.btn_bar);
         Button btn_parques = (Button) view.findViewById(R.id.btn_parques);
         Button btn_entretenimiento = (Button) view.findViewById(R.id.btn_entretenimiento);
+        FloatingActionButton fabBoton1=(FloatingActionButton) view.findViewById(R.id.fabBoton1);
        //RESTAURANTE
     btn_restaurante.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -71,6 +73,13 @@ public class CategoriaFragment extends Fragment {
                 Intent in = new  Intent(getActivity(), categoriaEntretenimientoActivity.class);
                 in.putExtra("Categoria", "Entretenimiento");
                 startActivity(in);
+            }
+        });
+        fabBoton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getActivity(),Main2Activity.class);
+                startActivity(i);
             }
         });
 
--- app/src/main/java/com/example/veronica/imgo/Main2Activity.java
+++ app/src/main/java/com/example/veronica/imgo/Main2Activity.java
@@ -1,13 +1,100 @@
 package com.example.veronica.imgo;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button btnShareLink,btnSharePhoto,btnShareVideo;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main2);
+
+        //Init View
+        btnShareLink=(Button)findViewById(R.id.btnShareLink);
+        btnSharePhoto=(Button)findViewById(R.id.btnSharePhoto);
+        btnShareVideo=(Button)findViewById(R.id.btnShareVideo);
+
+        //Init FB
+        callbackManager=CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
+        btnShareLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+                    @Override
+                    public void onSuccess(Sharer.Result result) {
+                        Toast.makeText(Main2Activity.this,"Share successful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(Main2Activity.this,"Share cancel", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Toast.makeText(Main2Activity.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setQuote("This is useful link ")
+                        .setContentUrl(Uri.parse("https://campus.ues.edu.sv/")).build();
+                if(ShareDialog.canShow(ShareLinkContent.class)){
+                    shareDialog.show(linkContent);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void printKeyHash(){
+        try {
+            PackageInfo info =getPackageManager().getPackageInfo("com.example.veronica.imgo",PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
